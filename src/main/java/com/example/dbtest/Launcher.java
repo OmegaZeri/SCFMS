d6a2f9f161b35c1a0ec2eb45dcfc34cd9092166f
OmegaZeri <camdentullis@gmail.com>
2026-01-28 11:04:42 -0600

    Twilio is now working fine (probably) however it does not throw the deprecated java activation api error now, however TWILIO still doesn't work beecause of Toll-Free-Verification B.S. on their side that I can't do

--- src/main/java/com/example/dbtest/Launcher.java
+++ src/main/java/com/example/dbtest/Launcher.java
@@ -1,9 +1,14 @@
 package com.example.dbtest;
 
+import com.twilio.Twilio;
 import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(SCFMSApplication.class, args);
+        Twilio.init("AC82eaebf56afcec16ba38bc16775dbabd", "b72b2705cda0f56836e032bafcde5bcf");
+
     }
+
+
 }
--- src/main/java/com/example/dbtest/SCFMSController.java
+++ src/main/java/com/example/dbtest/SCFMSController.java
@@ -1,5 +1,6 @@
 package com.example.dbtest;
 
+import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.MessageCreator;
 import javafx.fxml.FXML;
 import com.twilio.rest.api.v2010.account.Message;
@@ -8,7 +9,7 @@ import com.twilio.type.PhoneNumber;
 public class SCFMSController {
 
     @FXML public void textSend(){
-        MessageCreator twoFactorAuth = Message.creator(new PhoneNumber("+12058269239"),new PhoneNumber("+18559970149"), "Hello!");
+        MessageCreator twoFactorAuth = Message.creator(new PhoneNumber("+1"),new PhoneNumber("+1"), "Fuck Twilio");
         }
 
 }
